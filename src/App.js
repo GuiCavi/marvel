2cb998c66017d7e05805e722dc46ea169b03f57a
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2020-12-10 12:48:46 -0300

    Feat: Header and skeleton + spacing default values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import './App.css';
+import './App.sass';
 
-function App() {
-  return (
-    <div />
-  );
-}
+import LOGO from './assets/logo/logo.png';
+
+const App = () => (
+  <div className="container">
+    <header className="main-header">
+      <img src={LOGO} alt="" className="logo" />
+      <div className="person-details">
+        <div className="person-info">
+          <span className="bold">Guilherme Cavichioli</span>
+          <span>Teste de Front-end</span>
+        </div>
+
+        <div className="avatar">
+          <span className="bold">CB</span>
+        </div>
+      </div>
+    </header>
+
+    <h1>Busca de personagens</h1>
+
+    <div>
+      <label htmlFor="search">
+        Nome do personagem
+        <input type="text" id="search" />
+      </label>
+    </div>
+
+    <main>
+      <ul>
+        <li>
+          <img src="" alt="" />
+          <span>Abner Jenkins</span>
+        </li>
+      </ul>
+    </main>
+
+    <footer>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </footer>
+  </div>
+);
 
 export default App;
